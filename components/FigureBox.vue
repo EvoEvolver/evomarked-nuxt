0a8fbc5cee747456550ad037aff742aae807a09a
Zijian Zhang <doomspec@outlook.com>
2022-01-21 17:23:43 -0500

    Improve Figure/Remark Box to suit slides

--- components/FigureBox.vue
+++ components/FigureBox.vue
@@ -1,4 +1,12 @@
 <template>
+<RemarkBox name="Figure" :title="title" :id="id" :index="index" :hideHead="true">
+<div style="" class="figure-box">
+<slot name="img"></slot>
+</div>
+  <template v-slot:caption><slot></slot></template>
+</RemarkBox>
+
+<!--
   <figure :title="title" :id="id" :index="index">
   <slot name="img"></slot>
     <figcaption :index="index" :title="title" holder="Figure">
@@ -6,13 +14,23 @@
       <slot></slot>
     </figcaption>
   </figure>
+-->
 </template>
 
 <script>
+import RemarkBox from "./RemarkBox.vue";
 export default {
-  props: ["src", "id", "index", "title"]
+    props: ["src", "id", "index", "title"],
+    components: { RemarkBox }
 };
 </script>
 
 <style>
+.figure-box{
+  text-align: center;
+}
+.figure-box > img {
+  margin: auto;
+  max-height:25rem;
+}
 </style>
--- components/RemarkBox.vue
+++ components/RemarkBox.vue
@@ -1,15 +1,23 @@
 <template>
-  <div class="remark-box" :name="name" :title="title" :id="id" :index="index">
-    <span v-if="title" class="remark-title">{{ title }}</span>
+  <div class="remark-box">
+    <span v-if="!hideHead">
+      <span style="font-weight: bold">{{ name }} {{ index }}</span>
+      <span v-if="title" class="remark-title">{{ title }}</span>
+    </span>
     <p>
-      <slot />
+      <slot></slot>
     </p>
+    <div style="text-align:center;" v-if="this && this.$slots.caption" class="minor-text">
+    <div style="margin: auto;">
+      <p style="display:inline-block;">{{ name }} {{ index }}<span v-if="title" class="remark-title">{{ title }}</span>:&nbsp;</p><div style="display:inline-block;"><slot name="caption"></slot></div>
+    </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  props: ["title","id","name"],
+  props: ["title", "id", "name", "index", "hideHead"],
 };
 </script>
 

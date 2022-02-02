7e974de6bb34ff872071c5f0a6fc78bca3a8105b
Zijian Zhang <doomspec@outlook.com>
2022-02-02 13:53:08 -0500

    Introduce ProjectContentTable

---
+++ components/ProjectContentTable.vue
@@ -0,0 +1,16 @@
+<template>
+<div class="block" style="padding:1rem;">
+<h3>Pages</h3>
+<template v-for="(item, key) of pageEnv.linearize">
+<OuterRef :page="item"></OuterRef> <br/>
+</template>
+</div>
+</template>
+<script setup>
+import OuterRef from './OuterRef.vue';
+const pageEnv = inject('pageEnv')
+</script>
+<style lang="sass">
+@import "./Layout/vars"
+
+</style>

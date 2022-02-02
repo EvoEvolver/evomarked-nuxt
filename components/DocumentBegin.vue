82f8d33dedc959fb9fd9c6ace96ee524e880e551
Zijian Zhang <doomspec@outlook.com>
2022-02-02 13:49:55 -0500

    Move style to DocumentBegin

--- components/DocumentBegin.vue
+++ components/DocumentBegin.vue
@@ -10,10 +10,70 @@
 <script setup>
 import { inject } from 'vue'
 const pageEnv = inject('pageEnv')
-const { data: globalEnv } = await useAsyncData('globalEnv', () => $fetch('/page_assets/globalEnv.json'), { server: false })
-const reactiveEnv = inject("reactiveEnv")
-reactiveEnv.value.globalEnv = globalEnv
 </script>
 
-<style>
+<style lang="sass">
+@import "./Layout/vars"
+article
+    margin: auto
+    max-width: 50rem
+
+    padding-top: 1.5rem
+    padding-bottom: 5rem
+
+    font-family: "Times New Roman", Times, serif
+    font-size: 1.2rem
+body
+    margin: 0px
+    padding: 0px
+    -webkit-print-color-adjust: exact !important
+    
+
+@media (max-width: $small-screen-width)
+    article
+        padding-right: 0.3rem
+        padding-left: 0.3rem
+
+@media (min-width: $small-screen-width)
+    article
+        padding-right: 2rem
+        padding-left: 2rem
+
+.title
+    font-size: 2rem
+
+a
+    text-decoration: none
+    color: $link-color
+
+a:visited
+    color: $link-color
+
+h1
+    margin: 1rem
+    text-align: center
+    font-size: 1.7rem
+h2
+    margin: 1rem
+    text-align: center
+    font-size: 1.6rem
+h3, h4, h5, h6
+    margin: 1rem
+    text-align: center
+
+.katex
+    font-size: 1em !important
+
+.divider
+    text-align: center
+    font-size: 1.7em
+    margin: auto
+    display: block
+    &::before
+        content: "* * *"
+</style>
+
+<style src="./Layout/block.sass" lang="sass">
+</style>
+<style src="./Layout/inline.sass" lang="sass">
 </style>
--- components/Layout/LayoutDefault.vue
+++ components/Layout/LayoutDefault.vue
@@ -3,81 +3,15 @@
 </template>
 
 <script setup>
-import { provide } from "vue"
+import { provide, onMounted, reactive } from "vue"
 import katex from "katex/dist/katex.mjs";
 provide('katexAPI', katex);
-const reactiveEnv = ref({});
-provide('reactiveEnv',reactiveEnv);
+const { data:globalEnv } = await useAsyncData('globalEnv', () => $fetch('/page_assets/globalEnv.json'), { server: false })
+provide('globalEnv', globalEnv);
 </script>
 
-<style src="./block.sass" lang="sass">
-
-</style>
-<style src="./inline.sass" lang="sass">
-
-</style>
-
+<!-- Some style is in DocumentBegin-->
 <style lang="sass">
 
-@import "./vars"
-
-body
-    margin: 0px
-    padding: 0px
-    -webkit-print-color-adjust: exact !important
-
-article
-    margin: auto
-    max-width: 50rem
-    
-    padding-top: 1.5rem
-    padding-bottom: 5rem
-    
-    font-family: "Times New Roman", Times, serif
-    font-size: 1.2rem
-
-@media (max-width: $small-screen-width)
-    article
-        padding-right: 0.3rem
-        padding-left: 0.3rem
-
-@media (min-width: $small-screen-width)
-    article
-        padding-right: 2rem
-        padding-left: 2rem
-
-.title
-    font-size: 2rem
-
-a
-    text-decoration: none
-    color: $link-color
-
-a:visited
-    color: $link-color
-
-h1
-    margin: 1rem
-    text-align: center
-    font-size: 1.7rem
-h2
-    margin: 1rem
-    text-align: center
-    font-size: 1.6rem
-h3, h4, h5, h6
-    margin: 1rem
-    text-align: center
-
-.katex
-    font-size: 1em !important
-
-.divider
-    text-align: center
-    font-size: 1.7em
-    margin: auto
-    display: block
-    &::before
-        content: "* * *"
-
-
 </style>
+
